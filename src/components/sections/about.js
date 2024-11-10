db68c2ef1d7c2c5c3e35be746d9f17a7b8a8493a
Shruti <shrutisathi@berkeley.edu>
2024-11-10 13:07:05 -0800

    changing names and jobs

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,19 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = [
+    'C',
+    'Java',
+    'Python',
+    'GoLang',
+    'MySQL',
+    'Scheme',
+    'RISC-V',
+    'OpenMP',
+    'OpenMPI',
+    'HTML',
+    'CSS',
+  ];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,30 +147,10 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
-            </p>
-
-            <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
-            </p>
-
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              Hi, I'm Shruti, an Electrical Engineering and Computer Science student at UC Berkeley
+              with a passion for backend development and hardware testing. Over the years, I’ve
+              interned at tech companies like Reputation Inc. and Tarana Wireless, working on
+              projects from backend frameworks to digital board bring-up testing.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
